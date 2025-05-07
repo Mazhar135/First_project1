0a1d22749a873a322007658279c9dce5dd8018b7
Mazhar Hussain <mazharali10066@gmail.com>
2025-05-07 12:21:01 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      home:Scaffold(
+        backgroundColor: Colors.pink,
+    body:  Center(
+        child: Text("Hello world"))
+  )));
+}
